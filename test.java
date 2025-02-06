b23ff4e418df681e7b31b1b9d2f036f1ee72b89a
larlarua <90000267+larlarua@users.noreply.github.com>
2025-02-06 10:44:34 +0800

    Create test.java

---
+++ test.java
@@ -0,0 +1,7 @@
+// This is a simple Java program that prints "Hello, World!" to the console.
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
